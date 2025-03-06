dfe925ac862403f95c0cd3abc49a9fe715dfb992
Akira <caseyegging2009@gmail.com>
2025-03-06 14:00:23 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
 const button = document.getElementById("js--menu");
 const navigation = document.getElementById("js--nav");
 const body = document.getElementById("js--body");
-const li1 = document.getElementById("collection1")
-const li2 = document.getElementById("collection2")
-const li3 = document.getElementById("collection3")
-const li4 = document.getElementById("collection4")
 
 button.onclick = function() {
     navigation.style.visibility = "visible";
     body.style.overflow = "hidden";
-}
-
-li1.onclick = function() {
-    li1.style.
 }
